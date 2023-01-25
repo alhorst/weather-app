c261d436daeed323ac03e7114ffaf65bccda5699
Alexis Horst <alexisl.horst@gmail.com>
2023-01-25 13:02:41 -0500

    working on parsing the json still

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//put functions in 'weather' variable so we don't pollute global scope
 let weather = {
      apiGeo: "https://api.openweathermap.org/geo/1.0/direct?q=",
      apiWeather: "http://api.weatherapi.com/v1/",
@@ -11,42 +12,15 @@ let weather = {
                + "&q="
                + city
           )
-               .then((response) => {
-                    if (!response.ok) {
-                         alert("Weather not found at this location!");
-                         throw new Error("Weather not found!");
-                    }
-                    return response.json();
-               })
+               .then((response) => response.json())
                .then((data) => this.displayWeather(data));
      },
 
      displayWeather: function (data) {
-          const { name } = data;
-          const { text, icon } = data.condition[0];
-          const { temp_f, humidity } = data.current;
-          const { feels } = data.feelslike_f;
-
-          document.querySelector(".city").innerText = name;
-          document.querySelector(".icon").src = "//cdn.weatherapi.com/weather/64x64/" + icon + ".png";
-          document.querySelector(".description").innerText = text;
-          document.querySelector(".temp").innerText = temp_f + "°F";
-          document.querySelectorAll(".feels-like").innerText = feels;
-          document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-          document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
-     },
-
-     citySearch: function () {
-          this.fetchWeather(document.querySelector(".search_bar").value);
+          const { name } = data.location;
+          //const { icon, text } = data.condition; this is giving us undefined
+          const { temp_f, humidity, feelslike_f } = data.current;
+          console.log(name, temp_f, humidity, feelslike_f) //these are working
      }
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-     weather.citySearch();
-});
-
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-     if (event.key == "Enter") {
-          weather.citySearch();
-     }
-});
